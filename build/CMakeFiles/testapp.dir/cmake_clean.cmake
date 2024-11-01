ab083871ababb5b19d9fc3eb2851de96e7af6962
mgcvale <miguelcvalealt@gmail.com>
2024-11-01 20:07:44 -0300

    add connection pool and better epoll management

---
+++ build/CMakeFiles/testapp.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testapp.dir/app.c.o"
+  "CMakeFiles/testapp.dir/app.c.o.d"
+  "CMakeFiles/testapp.dir/sculpt_conn.c.o"
+  "CMakeFiles/testapp.dir/sculpt_conn.c.o.d"
+  "CMakeFiles/testapp.dir/sculpt_header.c.o"
+  "CMakeFiles/testapp.dir/sculpt_header.c.o.d"
+  "CMakeFiles/testapp.dir/sculpt_util.c.o"
+  "CMakeFiles/testapp.dir/sculpt_util.c.o.d"
+  "testapp"
+  "testapp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/testapp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
