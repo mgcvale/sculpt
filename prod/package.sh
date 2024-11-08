1b14361107c3897d6ba8afb6377bf7eea7b90277
mgcvale <miguelcvalealt@gmail.com>
2024-11-08 12:34:09 -0300

    Add bundling workflow

--- prod/package.sh
+++ prod/package.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-# Output file
 output="sculpt.c"
 
-# Clear the output file if it exists
 > "$output"
 
-# Add header file include (if needed)
 echo "// Combined source file for sculpt" >> "$output"
 echo "#include \"sculpt.h\"" >> "$output"
 echo "" >> "$output"
 
-# List of source files to merge
 src_files=(
     "../src/sculpt_header.c"
     "../src/sculpt_conn.c"
@@ -19,7 +15,6 @@ src_files=(
     "../src/sculpt_util.c"
 )
 
-# Iterate over each source file and append it to the output file
 for file in "${src_files[@]}"; do
     echo "// Start of $file" >> "$output"
     cat "$file" >> "$output"
